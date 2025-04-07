bd3ae4c70370f76e699f2bbf6d674814b2e7a320
Dan <62534871+MarshallD0@users.noreply.github.com>
2025-04-07 08:02:55 -0600

    Init

---
+++ src/types/db/house.ts
@@ -0,0 +1,15 @@
+export interface House {
+    price: number;
+    description: string;
+    area: number;
+    bedrooms: number;
+    bathrooms: number;
+    garage: boolean;
+    garden: boolean;
+    type: string;
+}
+
+
+interface House2{
+    price: number;
+}
